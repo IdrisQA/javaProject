df3ce717f7dba1207e41ad6a6e669d315ba21992
MumeenIdris <idris@herlabytes.com>
2018-02-02 14:41:33 +0100

    New added auto files

--- assignment1.java
+++ assignment1.java
@@ -1,7 +1,7 @@
 public class assignment1 {
     public static void main(String[] args) {
         System.out.println("My name is Mumeen Idris and I am boy willing to know java ");
-        System.out.println("I promise to work hard so as to achieve all what i've laboured for");
+        System.out.println("I promise to work hard");
     }
     
 }
