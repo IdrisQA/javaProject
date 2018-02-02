b1ca0abf7ec8ec21ec3629543cbd2a7b10d10fcc
MumeenIdris <idris@herlabytes.com>
2018-02-02 16:35:05 +0100

    Newly added

--- assignment1.java
+++ assignment1.java
@@ -1,7 +1,7 @@
 public class assignment1 {
     public static void main(String[] args) {
         System.out.println("My name is Mumeen Idris and I am boy willing to know java ");
-        System.out.println("I promise to work hard");
+        System.out.println("I promise to work hard and learn fast");
     }
     
 }
